eb95fde628ec95b04a3065c49d73ae0be0bb4a1b
Your Name <you@example.com>
2019-11-10 17:48:17 -0500

    Changing project structure

--- api/login/login.php
+++ api/login/login.php
@@ -1,5 +1,5 @@
 <?php
-require "conn.php";
+require "../util/conn.php";
 $email = $_POST['email'];
 $password = $_POST['password'];
 $mysql_query = "select * from user where email='$email' and password='$password';";
